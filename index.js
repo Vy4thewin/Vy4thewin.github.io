4fc09d80b5878a5ba4ee8d688aecad7241dffce1
Vy4thewin <vya.hill@gmail.com>
2021-03-22 22:35:48 -0400

    commit on 'main'

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ Created by Vyanna Hill
 var btnSign=document.getElementById("btnLog");
 var eMail=document.getElementById("email");
 var btnC=document.getElementById("btnLA");
-var welcome=document.getElementById("welcome")
+var signForm=document.getElementById("signForm");
+var welcome=document.getElementById("welcome");
+
 
 btnC.style.display='none';
 //hide all the sign in options
 
 btnSign.addEventListener("click",function(){
+    signForm.style.display="none";
     welcome.innerHTML="Hello,"+eMail;
-
+    btnC.style.display="inline";
 });
 
 
